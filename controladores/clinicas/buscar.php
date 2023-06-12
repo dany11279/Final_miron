a7abe2c50e7a403e363a39c521972789c8c75340
Dany <danymel1279@gmail.com>
2023-06-12 11:18:21 -0600

    CORREGI MI ARCHIVO BUSCAR

--- controladores/clinicas/buscar.php
+++ controladores/clinicas/buscar.php
@@ -1,7 +1,7 @@
 <?php
 require '../../modelos/Clinica.php';
 try {
-    $clinica = new Clinica($_GET);
+    $clinica = new cli($_GET);
     $clinicas = $clinica->buscar();
 } catch (PDOException $e) {
     $error = $e->getMessage();
@@ -16,7 +16,8 @@ try {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Resultados de busqueda</title>
 </head>
 <body>
@@ -51,7 +52,7 @@ try {
         </div>
         <div class="row justify-content-center">
             <div class="col-lg-4">
-                <a href="/Final_miron/vistas/clinicas/buscar.php" class="btn btn-info w-100">Regresar a la busqueda</a>
+                <a href="/Final_miron/vistas/clinicas/buscar.php" class="btn btn-info w-100">REGRESAR AL FORMULARIO</a>
             </div>
         </div>
     </div>
