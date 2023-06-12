de606389d6d379e285405a70e12df4da1db1713d
Dany <danymel1279@gmail.com>
2023-06-12 15:49:53 -0600

    termine de corregir todos mis errores

--- vistas/medicos/modificar.php
+++ vistas/medicos/modificar.php
@@ -1,37 +1,60 @@
 <?php
 require '../../modelos/Medico.php';
+require '../../modelos/Especialidad.php';
+require '../../modelos/Clinica.php';
 
-try {
-    if(isset($_GET['paciente_id']) && $_GET['paciente_id'] != ''){
 
-        $pac_id = $_GET['pac_id'];
-        $paciente = new Paciente(["pac_id" => $paciente_id]);
-        $pacientes = $paciente->buscar();}
+    try { if(isset($_GET['med_id']) && $_GET['med_id'] != ''){
+    
+        $med_id = $_GET['med_id'];
+        $medico = new Medico(["med_id" => $med_id]);
+        $especialidad = new Especialidad($_GET);
+        $clinica = new cli($_GET);
+
+
+        $medicos = $medico->buscar();
+        $especialidades = $especialidad->buscar();
+        $clinicas = $clinica->buscar();
     }
-
+    } catch (PDOException $e) {
+        $error = $e->getMessage();
+    } catch (Exception $e2){
+        $error = $e2->getMessage();
+    }
 ?>
 <?php include_once '../../includes/header.php'?>
+<?php include_once '../../includes/navbar.php'?>
     <div class="container">
-        <h1 class="text-center">Modificar medico</h1>
+        <h1 class="text-center">Modificar pacientes</h1>
         <div class="row justify-content-center">
             <form action="/Final_miron/controladores/medicos/modificar.php" method="POST" class="col-lg-8 border bg-light p-3">
-            <input type="hidden" name="pac_id" value="<?= $pacientes[0]['PAC_ID'] ?>">
+                <input type="hidden" name="med_id" value="<?= $medicos[0]['MED_ID'] ?>">
                 <div class="row mb-3">
                     <div class="col">
-                        <label for="med_nom">nombre del medico</label>
-                        <input type="text" name="med_nom" id="med_nom" class="form-control" required>
+                        <label for="med_nom">Nombre del medico</label>
+                        <input type="text" name="med_nom" id="med_nom" value="<?= $medicos[0]['MED_NOM'] ?>" class="form-control" required>
                     </div>
                 </div>
                 <div class="row mb-3">
                     <div class="col">
-                        <label for="med_esp">especialidades</label>
-                        <input type="text" name="med_esp" id="med_esp" class="form-control" required>
+                        <label for="med_esp">Especialidad</label>
+                        <select name="med_esp" id="med_esp" class="form-control">
+                            <option value="">SELECCIONE...</option>
+                            <?php foreach ($especialidades as $key => $especialidad) : ?>
+                                <option value="<?= $especialidad['ESP_ID'] ?>"><?= $especialidad['ESP_NOM'] ?></option>
+                            <?php endforeach?>
+                        </select>
                     </div>
                 </div>
                 <div class="row mb-3">
                     <div class="col">
-                        <label for="med_cli">clinica</label>
-                        <input type="text" name="med_cli" id="med_cli" class="form-control" required>
+                        <label for="med_cli">Clinica</label>
+                        <select name="med_cli" id="med_cli" class="form-control">
+                            <option value="">SELECCIONE...</option>
+                            <?php foreach ($clinicas as $key => $clinica) : ?>
+                                <option value="<?= $clinica['CLI_ID'] ?>"><?= $clinica['CLI_NOM'] ?></option>
+                            <?php endforeach?>
+                        </select>
                     </div>
                 </div>
                 <div class="row mb-3">
