ba9dec21fd6cd08a5e35ce555383a7881bec870e
Dany <danymel1279@gmail.com>
2023-06-12 03:19:46 -0600

    modifique un error en mi archivo guardar

--- controladores/citas/guardar.php
+++ controladores/citas/guardar.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 require '../../modelos/Cita.php';
 
 
-if($_POST['cita_paciente'] != '' && $_POST['cita_medico'] != '' && $_POST['cita_fecha'] != '' && $_POST['cita_hora'] != '' && $_POST['cita_referencia'] != ''){
+if($_POST['cita_pac'] != '' && $_POST['cita_med'] != '' && $_POST['cita_fecha'] != '' && $_POST['cita_hora'] != '' && $_POST['cita_referencia'] != ''){
 
 $_POST['cita_fecha']= date('d/m/Y', strtotime($_POST['cita_fecha']));
 
@@ -32,7 +32,8 @@ $_POST['cita_fecha']= date('d/m/Y', strtotime($_POST['cita_fecha']));
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Resultados</title>
 </head>
 <body>
