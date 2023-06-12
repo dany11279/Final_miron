3e5017d6913aa5eb492dfb5571937647940e3f16
Dany <danymel1279@gmail.com>
2023-06-12 01:45:28 -0600

    crie mi archivo de conexion

---
+++ modelos/Conexion.php
@@ -0,0 +1,41 @@
+<?php
+abstract class Conexion{
+    public static $conexion = null;
+
+    private static function conectar(){
+        try{
+            
+            self::$conexion = new PDO('informix:host=host.docker.internal; service=9088; 
+            database=hospital_programacion; server=informix; protocol=onsoctcp;EnableScrollableCursors = 1','informix','in4mix'); 
+            self::$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     
+        }catch(PDOException $e){
+            echo "Error de conexion en la Base de Datos";
+            echo "<br>";
+            echo $e->getMessage();
+            exit;
+        }
+
+        return self::$conexion;
+    }
+
+    public static function ejecutar($sql){
+        self::conectar();
+        $sentencia = self::$conexion->prepare($sql);
+        $resultado = $sentencia->execute();
+        $id = self::$conexion->lastInsertId();
+        self::$conexion = null;
+        return [
+            'resultado' => $resultado,
+            'id' => $id
+        ];
+    }
+
+    public static function servir($sql){
+        self::conectar();
+        $sentencia = self::$conexion->prepare($sql);
+        $sentencia->execute();
+        $resultados = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+        return $resultados;
+    }
+}
